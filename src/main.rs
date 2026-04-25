38f195d1b7ad07ec25778ee516e065badd153ccd
cyber-sapoperro <reclusive_coeditor@proton.me>
2026-04-24 21:58:30 -0500

    feat: Add basic api functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+fn rae_api(word: &str) -> Result<(), ureq::Error> {
+    let url = format!("https://rae-api.com/api/words/{}", word);
+    let body = ureq::get(&url)
+        .call()?
+        .body_mut()
+        .read_to_string()?;
+
+    println!("{}", body);
+
+    Ok(())
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let word: &str = &args[1];
+
+    if let Err(e) = rae_api(word) {
+        eprintln!("Error: {}", e);
+    }
 }
