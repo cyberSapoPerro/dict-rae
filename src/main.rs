72be8f07da5579478f839bcc7f9b2b9a048b36c1
cyber-sapoperro <reclusive_coeditor@proton.me>
2026-04-24 22:19:15 -0500

    feat(api): Move error handling to api function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-fn rae_api(word: &str) -> Result<(), ureq::Error> {
+fn rae_api(word: &str) {
     let url = format!("https://rae-api.com/api/words/{}", word);
-    let body = ureq::get(&url)
-        .call()?
-        .body_mut()
-        .read_to_string()?;
+
+    let mut response = match ureq::get(&url).call() {
+        Ok(resp) => resp,
+        Err(ureq::Error::StatusCode(code)) => {
+            eprintln!("HTTP error: {}", code);
+            return;
+        },
+        Err(e) => {
+            eprintln!("Request failed: {}", e);
+            return;
+        },
+    };
+
+    let body = match response.body_mut().read_to_string() {
+        Ok(b) => b,
+        Err(e) => {
+            eprintln!("Read error: {}", e);
+            return;
+        }
+    };
 
     println!("{}", body);
-
-    Ok(())
 }
 
 
@@ -15,7 +29,5 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let word: &str = &args[1];
 
-    if let Err(e) = rae_api(word) {
-        eprintln!("Error: {}", e);
-    }
+    rae_api(&word);
 }
